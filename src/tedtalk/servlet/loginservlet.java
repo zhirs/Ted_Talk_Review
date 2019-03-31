21a0d5c4d2c51d28ce7323f0acb5d023a6cb2d45
JosephLandau <jlandau2@ycp.edu>
2019-03-31 10:44:24 -0400

    Fixed illegal state exception coming from the login servlet.

--- src/tedtalk/servlet/loginservlet.java
+++ src/tedtalk/servlet/loginservlet.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import tedtalk.model.ProfileModel;
 import tedtalk.controller.ProfileController;
@@ -21,14 +22,13 @@ public class loginservlet extends HttpServlet {
 		username = (String) req.getSession().getAttribute("username");
 		// call JSP to generate empty form
 		if(username != null) {
-			resp.sendRedirect(req.getContextPath() + "/login");
 			req.getSession().setAttribute("username", null);
+			resp.sendRedirect(req.getContextPath() + "/login");
 		}
 		else {
 			req.getRequestDispatcher("/_view/login.jsp").forward(req, resp);
 		}
 		// call JSP to generate empty form
-		req.getRequestDispatcher("/_view/login.jsp").forward(req, resp);
 	}
 	
 	@Override
@@ -40,32 +40,26 @@ public class loginservlet extends HttpServlet {
 		ProfileModel model = new ProfileModel();
 		ProfileController controller = new ProfileController();
 		controller.setModel(model);
-		String errorMessage = null;
 		// set "game" attribute to the model reference
 		// the JSP will reference the model elements through "game"	
-		try {
-			String user = req.getParameter("u");
-			String pass = req.getParameter("p");
+		String user = req.getParameter("u");
+		String pass = req.getParameter("p");
 			
-			model.setUser(user);
-			model.setPass(pass);
+		model.setUser(user);
+		model.setPass(pass);
 			
-			if(controller.verified()) {
-				resp.sendRedirect(req.getContextPath() + "/home");
-				System.out.println("Login Servlet: Login Successful");
-				req.getSession().setAttribute("username", model.getUser());
-			}
-			else{
-				req.setAttribute("response", "Incorrect Username or Password");
-				System.out.println("Login Servlet: Login Failed");
-				req.getRequestDispatcher("/_view/login.jsp").forward(req, resp);
-			}
-		}catch (NumberFormatException e) {
-			errorMessage = "Invalid double";
+		if(controller.verified()) {
+			HttpSession session = req.getSession(true);
+			session.setAttribute("username", model.getUser());
+			System.out.println("Login Servlet: Login Successful");
+			resp.sendRedirect(req.getContextPath() + "/home");
 		}
+		else{
+			req.setAttribute("response", "Incorrect Username or Password");
+			System.out.println("Login Servlet: Login Failed");				
+			req.getRequestDispatcher("/_view/login.jsp").forward(req, resp);
+			}
 
-		// now call the JSP to render the new page
-		req.getRequestDispatcher("/_view/login.jsp").forward(req, resp);
+		// now call the JSP to render the new page	
 	}
-
 }
